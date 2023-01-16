82d272e4f5530d04834fdd9d28d5d61c65afd25a
DarukBey <dorukersoy2006@gmail.com>
2023-01-16 13:15:40 +0300

    Create Search&Sort.java

---
+++ week15/Search&Sort.java
@@ -0,0 +1,18 @@
+package week15;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class SearchSort {
+    public static int Search(int[] arr, int target) {
+        return 0;
+    }
+    public static void main(String[] args) {
+        List<Integer> List  = new ArrayList<Integer>();
+        //write a for loop that iterates 1 to 8
+        for (int i = 1; i <= 8; i++) {
+            List.add(i);
+        }
+        int index = 0;
+    }
+}
